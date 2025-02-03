72d4a954ed64570761890aa2521e9fdc9c688b32
Oshanath Rajawasam <oshanath.19@cse.mrt.ac.lk>
2025-02-03 23:29:53 +0530

    Initial commit

---
+++ VulkanTemplate.cpp
@@ -0,0 +1,136 @@
+﻿#define VK_USE_PLATFORM_WIN32_KHR
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#define GLFW_EXPOSE_NATIVE_WIN32
+#include <GLFW/glfw3native.h>
+#include <vulkan/vulkan.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
+#include <iostream>
+
+#include "VkBootstrap.h"
+
+static VKAPI_ATTR VkBool32 VKAPI_CALL debug_callback(
+    VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+    VkDebugUtilsMessageTypeFlagsEXT messageType,
+    const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+    void* pUserData) {
+
+    std::cerr << "Validation layer: " << pCallbackData->pMessage << std::endl;
+    return VK_FALSE;
+}
+
+int main() {
+    // Create window
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    std::cout << extensionCount << " extensions supported\n";
+
+    // Create instance
+    vkb::InstanceBuilder builder;
+    auto inst_ret = builder.set_app_name("Example Vulkan Application")
+        .enable_validation_layers()
+		.use_default_debug_messenger()
+        .build();
+    if (!inst_ret) {
+        std::cerr << "Failed to create Vulkan instance. Error: " << inst_ret.error().message() << "\n";
+        return 1;
+    }
+    vkb::Instance vkb_inst = inst_ret.value();
+
+    // Create Surface
+    VkSurfaceKHR surface;
+    VkWin32SurfaceCreateInfoKHR createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR;
+    createInfo.hwnd = glfwGetWin32Window(window);
+    createInfo.hinstance = GetModuleHandle(nullptr);
+    if (vkCreateWin32SurfaceKHR(vkb_inst.instance, &createInfo, nullptr, &surface) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create window surface!");
+    }
+
+    vkb::PhysicalDeviceSelector selector{ vkb_inst };
+    auto phys_ret = selector.set_surface(surface)
+        .set_minimum_version(1, 1) // require a vulkan 1.1 capable device
+        .require_dedicated_transfer_queue()
+        .select();
+    if (!phys_ret) {
+        std::cerr << "Failed to select Vulkan Physical Device. Error: " << phys_ret.error().message() << "\n";
+        return 1;
+    }
+
+    vkb::DeviceBuilder device_builder{ phys_ret.value() };
+    // automatically propagate needed data from instance & physical device
+    auto dev_ret = device_builder.build();
+    if (!dev_ret) {
+        std::cerr << "Failed to create Vulkan device. Error: " << dev_ret.error().message() << "\n";
+        return 1;
+    }
+    vkb::Device vkb_device = dev_ret.value();
+
+    // Get the VkDevice handle used in the rest of a vulkan application
+    VkDevice device = vkb_device.device;
+
+    // Get the graphics queue with a helper function
+    auto graphics_queue_ret = vkb_device.get_queue(vkb::QueueType::graphics);
+    if (!graphics_queue_ret) {
+        std::cerr << "Failed to get graphics queue. Error: " << graphics_queue_ret.error().message() << "\n";
+        return 1;
+    }
+    VkQueue graphics_queue = graphics_queue_ret.value();
+
+    // Create Swapchain
+    vkb::SwapchainBuilder swapchain_builder{ vkb_device };
+    auto swap_ret = swapchain_builder.set_desired_extent(700, 500).build();
+    if (!swap_ret) {
+
+    }
+    vkb::Swapchain swapchain = swap_ret.value();
+
+    // --------------------------------------------------------------------------------
+    VkCommandPool commandPool;
+    VkCommandBuffer commandBuffer;
+
+    VkCommandPoolCreateInfo poolInfo{};
+    poolInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+    poolInfo.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
+    poolInfo.queueFamilyIndex = 0;
+
+    if (vkCreateCommandPool(device, &poolInfo, nullptr, &commandPool) != VK_SUCCESS) {
+        throw std::runtime_error("Failed to create command pool!");
+    }
+
+    VkCommandBufferAllocateInfo allocInfo{};
+    allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+    allocInfo.commandPool = commandPool;
+    allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+    allocInfo.commandBufferCount = 1;
+
+    if (vkAllocateCommandBuffers(device, &allocInfo, &commandBuffer) != VK_SUCCESS) {
+        throw std::runtime_error("Failed to allocate command buffer!");
+    }
+
+    VkSubmitInfo submitInfo{};
+    submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+    submitInfo.commandBufferCount = 1;
+    submitInfo.pCommandBuffers = &commandBuffer; // commandBuffer not recorded
+
+    vkQueueSubmit(graphics_queue, 1, &submitInfo, VK_NULL_HANDLE);
+
+    // --------------------------------------------------------------------------------
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+}
